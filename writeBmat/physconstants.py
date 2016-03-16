2e001ffdad8d4bb8827e28e988712fabd7e5b135
Lukasz Mentel <lmmentel@gmail.com>
2016-03-16 09:03:51 +0100

    updated the physonstants to use values from sciy.constants

--- writeBmat/physconstants.py
+++ writeBmat/physconstants.py
@@ -1,13 +1,13 @@
 
+from scipy.constants import value, angstrom
 
 class physConstants:
- def __init__(self):
-   self.Hartree2eV=27.2113834
-   self.AU2A=0.5291772083
-   self.ev2j=1.60217733e-19
-   self.bolkEV=8.6173857e-5
-   self.bolk=self.bolkEV*self.ev2j
-   self.planck=6.62608e-34
-   self.amutokg=1.66053886e-27
-   self.cl=2.997925e08
-   
+    def __init__(self):
+        self.Hartree2eV = value('Hartree energy in eV')
+        self.AU2A = value('atomic unit of length')/angstrom
+        self.ev2j = value('electron volt-joule relationship')
+        self.bolkEV = value('Boltzmann constant in eV/K')
+        self.bolk = value('Boltzmann constant')
+        self.planck = value('Planck constant')
+        self.amutokg = value('atomic mass constant')
+        self.cl = value('speed of light in vacuum')
