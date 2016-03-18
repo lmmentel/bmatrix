a108b3a878a32d014b307852d2dd8db4d9fd2439
Lukasz Mentel <lmmentel@gmail.com>
2016-03-18 03:55:53 +0100

    removed the dependance on the oldnumeric and replaced it with numpy in writeBmat.py removed the dependance on old input processing module in writeBmat

--- writeBmat/writeBmat.py
+++ writeBmat/writeBmat.py
@@ -6,16 +6,17 @@ import time
 
 import numpy as np
 
-from numpy.oldnumeric import *
-from numpy.oldnumeric.linear_algebra import *
 from math import *
 from string import *
 from fpformat import *
 
-import bmatrix,intcoord
-import takeinpPOSCAR,various,mymath,dealxyz
+import bmatrix
+import intcoord
+import takeinpPOSCAR, various, mymath, dealxyz
 import datastruct
-import inputer
+import inputparser
+
+from pprint import pprint
 
 #from joblib import Parallel, delayed
 #import multiprocessing
@@ -38,7 +39,7 @@ MYTINY=1e-6
 def main():
     'Main program'
 
-    args = inputer.get_input_args()
+    args = inputparser.get_input_args()
 
     inpt = takeinpPOSCAR.TakeInput()
     inpt.read(args.filename)
@@ -50,21 +51,19 @@ def main():
     volume = inpt.volume
     atomquality = inpt.atomicFlags
 
-    if len(inps_.ATRADII)!=len(atomquality):
-        inps_.ATRADII=[]
-        COVALENTRADII=datastruct.Elementprop().covalentradii
+    if len(args.atradii) != len(atomquality):
+        args.atradii = []
+        COVALENTRADII = datastruct.Elementprop().covalentradii
     for i in range(len(atomquality)):
         index=atomquality[i]
-        inps_.ATRADII.append(COVALENTRADII[index])
+        args.atradii.append(COVALENTRADII[index])
     atomictags=[]
     for i in range(inpt.ntypes):
         for j in range(inpt.types[i]):
             atomictags.append(atomquality[i])
 
-    FRAG=inps_.FRAGCOORD
-
-    crt=various.change_format(inpt.coords_c)
-    cartesian = zeros(len(crt) + 9, Float)
+    crt = various.change_format(inpt.coords_c)
+    cartesian = np.zeros(len(crt) + 9, dtype=float)
     cartesian[:-9] = crt
     cartesian[-9:-6] = lattmat[0]
     cartesian[-6:-3] = lattmat[1]
@@ -76,42 +75,46 @@ def main():
 
     try:
         primcoords=read_matrix('COORDINATES.gadget')
-        deal=dealxyz.Dealxyz(cartesian[:-9],primcoords,lattmat)
+        deal=dealxyz.Dealxyz(cartesian[:-9], primcoords, lattmat)
         for i in range(len(primcoords)):
             primcoords[i].value=deal.internals[i]
         print 'Internal coordinates were read-in from the file COORDINATES.gadget'
     except IOError:
         print 'Internal coordinates were newly generated'
-        intrn=intcoord.Intern(atomquality,inps_.ATRADII,inps_.ASCALE,inps_.BSCALE,inps_.ANGLECRIT,inps_.TORSIONCRIT,FRAG,inps_.RELAX,inps_.TORSIONS,inps_.SUBST,inpt.numofatoms,inpt.lattmat,inpt.coords_d,inpt.coords_c,inpt.types)
-        primcoords=intrn.internalcoords
-        write_matrix(primcoords,'COORDINATES.gadget')
+        intrn = intcoord.Intern(atomquality, args.atradii, args.ascale, args.bscale, args.anglecrit,
+                                args.torsioncrit, args.fragcoord, args.relax, args.torsions,
+                                args.subst, inpt.numofatoms, inpt.lattmat, inpt.coords_d,
+                                inpt.coords_c, inpt.types)
+        primcoords = intrn.internalcoords
+        write_matrix(primcoords, 'COORDINATES.gadget')
 
     print time.time() - t0, "seconds wall time coord check"
 
     #c now compute the Bmatrix (wrt. fractional coordinates!)
-    b = bmatrix.bmatrix(cartesian[:-9], primcoords, inpt.numofatoms, lattmat, inps_.RELAX)
+    b = bmatrix.bmatrix(cartesian[:-9], primcoords, inpt.numofatoms, lattmat, args.relax)
     Bmat = b.Bmatrix
-    #b=bmatrix_cart.bmatrix(cartesian[:-9],primcoords,inpt.numofatoms,lattmat,inps_.RELAX)
+    #b=bmatrix_cart.bmatrix(cartesian[:-9],primcoords,inpt.numofatoms,lattmat,args.RELAX)
     #Bmat_c=b.Bmatrix
 
     #c compute the Bmatrix wrt. Cartesian coordinates
     t0 = time.time()
 
-    if inps_.RELAX==0:
-        transmat=mymath.cd_transmatrix(lattinv,3*inpt.numofatoms)
-    else:
-        transmat=zeros((3*inpt.numofatoms+9,3*inpt.numofatoms+9),Float)
+    if args.relax:
+        transmat = np.zeros((3*inpt.numofatoms+9, 3*inpt.numofatoms+9), dtype=float)
         #  Parallel(n_jobs=num_cores)(delayed(processInput)(i)for i in range(3*inpt.numofatoms+9))
         #  transmat[i][i]=1.
-        for i in range(3*inpt.numofatoms+9): transmat[i][i]=1.
-        transmat[0:3*inpt.numofatoms,0:3*inpt.numofatoms]=mymath.cd_transmatrix(lattinv,3*inpt.numofatoms)
+        for i in range(3*inpt.numofatoms + 9):
+            transmat[i][i] = 1.0
+        transmat[0:3*inpt.numofatoms, 0:3*inpt.numofatoms] = mymath.cd_transmatrix(lattinv, 3*inpt.numofatoms)
+    else:
+        transmat = mymath.cd_transmatrix(lattinv, 3*inpt.numofatoms)
 
     print time.time() - t0, "seconds wall time for Bmat computation"
 
 
     t0 = time.time()
     #Bmat_c2=matrixmultiply(Bmat,transmat)
-    Bmat_c2 = dot(Bmat,transmat)
+    Bmat_c2 = np.dot(Bmat, transmat)
     print time.time() - t0, "seconds wall time for Bmat multiplication"
 
     if args.coordinates == 'cartesian':
@@ -160,7 +163,7 @@ def main():
     f.write(row+'\n')
     for i in range(len(Bmat)):
         for j in range(len(Bmat[0])):
-            if abs(Bmat[i][j]) >=MYTINY:
+            if abs(Bmat[i][j]) >= MYTINY:
                 row=str(i+1)+'\t'+str(j+1)+'\t'+str(Bmat[i][j])
                 f.write(row+'\n')
 
