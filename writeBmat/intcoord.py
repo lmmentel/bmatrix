77143fd8a06896d65ba2ab313c7833d49bd3f94f
Lukasz Mentel <lmmentel@gmail.com>
2016-05-13 20:04:15 +0200

    corrected some indentation issues in intcoords

--- writeBmat/intcoord.py
+++ writeBmat/intcoord.py
@@ -48,7 +48,7 @@ class Intern:
    for i in range(len(directs)):
      intrawhat.append(i)
      intrawhere.append(np.array([0,0,0]))
-     
+
    # intercell parameters
    interdirects,interwhat,interwhere=self.inter_search(directs,self.criteria)
 
@@ -71,28 +71,27 @@ class Intern:
    shortradii=0.2*np.array(atradii)                         # minimal lengths
    longradii=np.array(atradii)                        # upper limit for bond length
 
-   bonds=self.bond_lengths(cartesian,intrawhat,intrawhere,\
-   allcartesian,allwhat,allwhere,shortradii,longradii,katoms,'R')
+   bonds = self.bond_lengths(cartesian, intrawhat, intrawhere,\
+           allcartesian, allwhat, allwhere, shortradii, longradii, katoms, 'R')
 
    fragments,substrate=self.frac_struct(bonds,numofatoms,SUBST)
-    
+
    if len(bonds)==0:
      print 'are you sure about the at. rad.?'
 
-   angles,iangwhat,iangwhere=self.set_angles(bonds,directs,\
-   lattmat,numofatoms,'A',ANGLECRIT)
+   angles, iangwhat, iangwhere = self.set_angles(bonds,directs,\
+                                 lattmat,numofatoms,'A',ANGLECRIT)
 
+   self.internalcoords = []
+   self.longinternalcoords = []
 
-   self.internalcoords=[]
-   self.longinternalcoords=[]
-
-   if TORS==1:
-     torsions=self.set_dihedrals(iangwhat,iangwhere,directs,lattmat,numofatoms,'T',TORSIONCRIT)
-     self.internalcoords=bonds +angles+torsions
+   if TORS == 1:
+     torsions = self.set_dihedrals(iangwhat,iangwhere,directs,lattmat,numofatoms,'T',TORSIONCRIT)
+     self.internalcoords = bonds + angles + torsions
    else:
-     self.internalcoords=bonds  +angles
-   self.bonds=bonds
-   self.angles=angles
+     self.internalcoords = bonds + angles
+   self.bonds = bonds
+   self.angles = angles
 
 #########################################################################
    if len(fragments)>1:
@@ -114,11 +113,11 @@ class Intern:
      if RELAX==1:
        for i in range(3):
          singles.append(datastruct.Complextype('simple',[1],'hX',[i],[None],\
-	 [[0,0,0]],lattmat[i][0],'free'))
+             [[0,0,0]],lattmat[i][0],'free'))
          singles.append(datastruct.Complextype('simple',[1],'hY',[i],[None],\
-	 [[0,0,0]],lattmat[i][1],'free'))
+             [[0,0,0]],lattmat[i][1],'free'))
          singles.append(datastruct.Complextype('simple',[1],'hZ',[i],[None],\
-	 [[0,0,0]],lattmat[i][2],'free'))
+             [[0,0,0]],lattmat[i][2],'free'))
      self.internalcoords+=singles
 
    if (len(fragments)>1 or len(substrate)>0) and FRAGCOORD!=0:     
@@ -167,6 +166,7 @@ class Intern:
    criteria[2]=abs(2*atrad/cang32)
    return np.array(criteria)
 
+
  def make_exclusions(self,directs,which,criteria):
    """ Excludes those data from row_d, which do not satisfy the criteria
    which = 0,1,2 i.e. a b c; .
@@ -185,6 +185,7 @@ class Intern:
        nexclusions=nexclusions+[i]
 
    return pexclusions,nexclusions
+
 
  def multy_cell(self,directs,first,second,third,pexcluded,mexcluded):       #first, second, third - translations +/- 1
    """Consideres atoms outside cell which can form intercell bonds.
@@ -212,11 +213,13 @@ class Intern:
        intwhere.append(np.array([first,second,third]))
    return intdirects,intwhat,intwhere
 
+
  def dirto_cart(self,directs,lattmat):
    """Conversion from direct to cartesian coords.
    """
    carts=np.dot(directs,lattmat)
    return carts
+
 
  def inter_search(self,directs,criteria):
    'group of those atoms which can not form intercell bonds'
@@ -232,11 +235,12 @@ class Intern:
      for j in (-1,0,1):
        for k in (-1,0,1):
          if (i**2+j**2+k**2)!=0:
-	   idirects,iwhat,iwhere=self.multy_cell(directs,i,j,k,pexcluded,mexcluded)
+           idirects,iwhat,iwhere=self.multy_cell(directs,i,j,k,pexcluded,mexcluded)
            interdirects=interdirects+idirects
            interwhat=interwhat+iwhat
            interwhere=interwhere+iwhere
    return interdirects,interwhat,interwhere
+
 
  def bond_fragments(self,fragments,substrate,directs,lattmat,radii_l,katoms,tag):
    bonds=[]
@@ -255,73 +259,73 @@ class Intern:
      a=directs[substrate[jj]]
      for i in range(len(fragments)):
        for ii in range(len(fragments[i])):
-	 target=0
+         target=0
          while fragments[i][ii]>=katoms[target]:
            target=target+1
          radii2_l=radii_l[target]
          criteria_l=(radii1_l+radii2_l)
-	 b_=directs[fragments[i][ii]]
-	 for t1 in (-1,0,1):
-	   b=np.zeros(3,dtype=float)
-	   b[0]=b_[0]+t1
-	   for t2 in (-1,0,1):
+         b_=directs[fragments[i][ii]]
+         for t1 in (-1,0,1):
+           b=np.zeros(3,dtype=float)
+           b[0]=b_[0]+t1
+           for t2 in (-1,0,1):
              b[1]=b_[1]+t2
              for t3 in (-1,0,1):
-	       b[2]=b_[2]+t3
-	       r=b-a
-	       r=np.dot(r,lattmat)
-	       r=sum(r*r)**0.5
-	       if r<=criteria_l:
-		 ibonds.append(r)
-	         ibondwhat.append([substrate[jj],fragments[i][ii]])
+               b[2]=b_[2]+t3
+               r=b-a
+               r=np.dot(r,lattmat)
+               r=sum(r*r)**0.5
+               if r<=criteria_l:
+                 ibonds.append(r)
+                 ibondwhat.append([substrate[jj],fragments[i][ii]])
                  ibondwhattags.append([self.atomictags[substrate[jj]],self.atomictags[fragments[i][ii]]])
-	         ibondwhere.append([[0,0,0],[t1,t2,t3]])
-   
-   
+                 ibondwhere.append([[0,0,0],[t1,t2,t3]])
+
+
    for i in range(len(fragments)):
      for j in range(len(fragments)):
        if j>i:
        #if j>=i: 
-	 for ii in range(len(fragments[i])):
-	   target=0
+         for ii in range(len(fragments[i])):
+           target=0
            while fragments[i][ii]>=katoms[target]:
              target=target+1
            radii1_l=radii_l[target]
-	   a=directs[fragments[i][ii]]
-	   for jj in range(len(fragments[j])):
-	     if fragments[j][jj]!=fragments[i][ii]:
-	       target=0
+           a=directs[fragments[i][ii]]
+           for jj in range(len(fragments[j])):
+             if fragments[j][jj]!=fragments[i][ii]:
+               target=0
                while fragments[j][jj]>=katoms[target]:
                  target=target+1
                radii2_l=radii_l[target]
                criteria_l=(radii1_l+radii2_l)
-	       b_=directs[fragments[j][jj]]
-	       for t1 in (-1,0,1):
-	         b=np.zeros(3,dtype=float)
-	         b[0]=b_[0]+t1
-	         for t2 in (-1,0,1):
-		   b[1]=b_[1]+t2
+               b_=directs[fragments[j][jj]]
+               for t1 in (-1,0,1):
+                 b=np.zeros(3,dtype=float)
+                 b[0]=b_[0]+t1
+                 for t2 in (-1,0,1):
+                   b[1]=b_[1]+t2
                    for t3 in (-1,0,1):
-		     b[2]=b_[2]+t3
-		     r=b-a
-		     r=np.dot(r,lattmat)
-		     r=sum(r*r)**0.5
-		     if r<=criteria_l:
-		       ibonds.append(r)
-	               ibondwhat.append([fragments[i][ii],fragments[j][jj]])
+                     b[2]=b_[2]+t3
+                     r=b-a
+                     r=np.dot(r,lattmat)
+                     r=sum(r*r)**0.5
+                     if r<=criteria_l:
+                       ibonds.append(r)
+                       ibondwhat.append([fragments[i][ii],fragments[j][jj]])
                        ibondwhattags.append([self.atomictags[fragments[i][ii]],self.atomictags[fragments[j][jj]]])
-	               ibondwhere.append([[0,0,0],[t1,t2,t3]])
+                       ibondwhere.append([[0,0,0],[t1,t2,t3]])
    ksort=[]
    for i in range(len(ibonds)):
      kk=[ibondwhat[i][0],i]
      ksort.append(kk)
    ksort.sort()
-   
+
    for i in range(len(ksort)):
      bindex=ksort[i][1]
      bonds.append(datastruct.Complextype('simple',[1],tag,ibondwhat[bindex],\
      ibondwhattags[bindex],ibondwhere[bindex],ibonds[bindex],'free'))
-   return bonds	   
+   return bonds
   
   
  def bond_lengths(self,cart,what,where,allcart,allwhat,allwhere,radii_s,radii_l,katoms,tag):
@@ -338,23 +342,23 @@ class Intern:
        length=mymath.vector_size(diffvec)
        target=0
        while what[i]>=katoms[target]:
-	 target=target+1
+         target=target+1
 
        radii1_s=radii_s[target]
        radii1_l=radii_l[target]
 
        target=0
        while allwhat[j]>=katoms[target]:
-	 target=target+1
+         target=target+1
        radii2_s=radii_s[target]
        radii2_l=radii_l[target]
        criteria_s=(radii1_s+radii2_s)
        criteria_l=(radii1_l+radii2_l)
        if length>criteria_s and length<=criteria_l and what[i]<=allwhat[j]:
-	 ibonds.append(length)
-	 ibondwhat.append([what[i],allwhat[j]])
+         ibonds.append(length)
+         ibondwhat.append([what[i],allwhat[j]])
          ibondwhattags.append([self.atomictags[what[i]],self.atomictags[allwhat[j]]])
-	 ibondwhere.append([where[i],allwhere[j]])
+         ibondwhere.append([where[i],allwhere[j]])
    ksort=[]
    for i in range(len(ibonds)):
      kk=[ibondwhat[i][0],i]
@@ -401,32 +405,32 @@ class Intern:
      for k in range(len(vectors)):
        for l in range(len(vectors)):
          #if l<k:
-	 if l<k and iangwhat[i][k]!=i and iangwhat[i][l]!=i:
+         if l<k and iangwhat[i][k]!=i and iangwhat[i][l]!=i:
          #if l<k and iangwhat[i][k]!=iangwhat[i][l]:   # angles containing one atom twice -this has to be solved
            angle=(sum(vectors[k][0]*vectors[l][0]))/(vectors[k][1]*vectors[l][1])
-	   if angle>1: angle=1
-	   if angle<-1:angle=-1
-	   angle=abs(acos(angle))
-	   #if not(sum(self.topology_matrix[iangwhat[i][k],:])>6 and sum(self.topology_matrix[iangwhat[i][l],:])>6 and sum(self.topology_matrix[i,:])>6):
-			   
-	   if sin(angle) >self.trust:
-	       if iangwhat[i][k]<iangwhat[i][l] :
-	         awhat=[iangwhat[i][k],i,iangwhat[i][l]]
-                 awhattags=[self.atomictags[iangwhat[i][k]],self.atomictags[i],self.atomictags[iangwhat[i][l]]]
-	         awhere=[iangwhere[i][k],[0,0,0],iangwhere[i][l]]
-               else:
-	         awhat=[iangwhat[i][l],i,iangwhat[i][k]]
-	         awhattags=[self.atomictags[iangwhat[i][l]],self.atomictags[i],self.atomictags[iangwhat[i][k]]]
-	         awhere=[iangwhere[i][l],[0,0,0],iangwhere[i][k]]
+           if angle>1: angle=1
+           if angle<-1:angle=-1
+           angle=abs(acos(angle))
+           # if not(sum(self.topology_matrix[iangwhat[i][k],:])>6 and sum(self.topology_matrix[iangwhat[i][l],:])>6 and sum(self.topology_matrix[i,:])>6):
+
+           if sin(angle) >self.trust:
+             if iangwhat[i][k]<iangwhat[i][l] :
+               awhat=[iangwhat[i][k],i,iangwhat[i][l]]
+               awhattags=[self.atomictags[iangwhat[i][k]],self.atomictags[i],self.atomictags[iangwhat[i][l]]]
+               awhere=[iangwhere[i][k],[0,0,0],iangwhere[i][l]]
+             else:
+               awhat=[iangwhat[i][l],i,iangwhat[i][k]]
+               awhattags=[self.atomictags[iangwhat[i][l]],self.atomictags[i],self.atomictags[iangwhat[i][k]]]
+               awhere=[iangwhere[i][l],[0,0,0],iangwhere[i][k]]
                angles.append(datastruct.Complextype('simple',[1],tag,awhat,awhattags,awhere,angle,'free'))
-	   else:
-               continue
-	       #awhat=[iangwhat[i][k],iangwhat[i][l]]
-               #awhattags=[self.atomictags[iangwhat[i][k]],self.atomictags[iangwhat[i][l]]]
-	       #awhere=[iangwhere[i][k],iangwhere[i][l]]
-               #emerbonds.append(datastruct.Complextype('simple',[1],'R',awhat,awhattags,\
-               #awhere,None,'free'))
-	       #print 'stretched',i,awhat,awhere
+           else:
+             continue
+             # awhat=[iangwhat[i][k],iangwhat[i][l]]
+             # awhattags=[self.atomictags[iangwhat[i][k]],self.atomictags[iangwhat[i][l]]]
+             # awhere=[iangwhere[i][k],iangwhere[i][l]]
+             # emerbonds.append(datastruct.Complextype('simple',[1],'R',awhat,awhattags,\
+             # awhere,None,'free'))
+             # print 'stretched',i,awhat,awhere
 
    for i in range(len(emerbonds)):
      indx1=emerbonds[i].what[0]
@@ -435,20 +439,20 @@ class Intern:
      for j in range(i):
        if emerbonds[i].what[0]==emerbonds[j].what[0] and \
           emerbonds[i].what[1]==emerbonds[j].what[1]:
-	 if emerbonds[i].where[0][0]==emerbonds[j].where[0][0] and \
+          if emerbonds[i].where[0][0]==emerbonds[j].where[0][0] and \
             emerbonds[i].where[0][1]==emerbonds[j].where[0][1] and \
             emerbonds[i].where[0][2]==emerbonds[j].where[0][2] and \
             emerbonds[i].where[1][0]==emerbonds[j].where[1][0] and \
             emerbonds[i].where[1][1]==emerbonds[j].where[1][1] and \
             emerbonds[i].where[1][2]==emerbonds[j].where[1][2]:
-	    exitus=1
+            exitus=1
      if exitus==1:
        continue
      for j in range(len(iangwhat[indx1])):
        if iangwhat[indx1][j]<indx2:
          awhat=[iangwhat[indx1][j],indx1,indx2]
          awhattags=[self.atomictags[iangwhat[indx1][j]],self.atomictags[indx1],self.atomictags[indx2]]
-	 awhere=[iangwhere[indx1][j],[0,0,0],emerbonds[i].where[1]-emerbonds[i].where[0]]
+         awhere=[iangwhere[indx1][j],[0,0,0],emerbonds[i].where[1]-emerbonds[i].where[0]]
        else:
          awhat=[indx2,indx1,iangwhat[indx1][j]]
          awhattags=[self.atomictags[indx2],self.atomictags[indx1],self.atomictags[iangwhat[indx1][j]]]
@@ -461,7 +465,7 @@ class Intern:
        if iangwhat[indx2][j]<indx1:
          awhat=[iangwhat[indx2][j],indx2,indx1]
          awhattags=[self.atomictags[iangwhat[indx2][j]],self.atomictags[indx2],self.atomictags[indx1]]
-	 awhere=[iangwhere[indx2][j],[0,0,0],emerbonds[i].where[0]-emerbonds[i].where[1]]
+         awhere=[iangwhere[indx2][j],[0,0,0],emerbonds[i].where[0]-emerbonds[i].where[1]]
        else:
          awhat=[indx1,indx2,iangwhat[indx2][j]]
          awhattags=[self.atomictags[indx1],self.atomictags[indx2],self.atomictags[iangwhat[indx2][j]]]
@@ -493,34 +497,34 @@ class Intern:
        secondwhere=np.array([0,0,0])
        for j in range(len(iangwhat[i])):
          firstwhat=iangwhat[i][j]
-	 #if (len(self.topmap[secondwhat])>4 and len(self.topmap[firstwhat])>4):continue
+         # if (len(self.topmap[secondwhat])>4 and len(self.topmap[firstwhat])>4):continue
          if (len(self.topmap[secondwhat])>TORSIONCRIT and len(self.topmap[firstwhat])>TORSIONCRIT):continue
-	 if firstwhat==secondwhat:continue
-	 firstwhere=iangwhere[i][j]
-	 for k in range(len(iangwhat[i])):
-	   if k!=j and iangwhat[i][k]>secondwhat:
+         if firstwhat==secondwhat:continue
+         firstwhere=iangwhere[i][j]
+         for k in range(len(iangwhat[i])):
+           if k!=j and iangwhat[i][k]>secondwhat:
              thirdwhat=iangwhat[i][k]
-	     if thirdwhat==firstwhat or thirdwhat==secondwhat:continue
-	     thirdwhere=iangwhere[i][k]
-	 #    if len(self.topmap[thirdwhat])>4:continue
-	     if (len(self.topmap[thirdwhat])>4 and len(self.topmap[firstwhat])>4):continue
-	     for l in range(len(iangwhat[thirdwhat])):
-	       fourthwhat=iangwhat[thirdwhat][l]
-	       if fourthwhat==firstwhat or fourthwhat==secondwhat or fourthwhat==thirdwhat:continue
-	       fourthwhere=iangwhere[thirdwhat][l]+thirdwhere
+             if thirdwhat==firstwhat or thirdwhat==secondwhat:continue
+             thirdwhere=iangwhere[i][k]
+             # if len(self.topmap[thirdwhat])>4:continue
+             if (len(self.topmap[thirdwhat])>4 and len(self.topmap[firstwhat])>4):continue
+             for l in range(len(iangwhat[thirdwhat])):
+               fourthwhat=iangwhat[thirdwhat][l]
+               if fourthwhat==firstwhat or fourthwhat==secondwhat or fourthwhat==thirdwhat:continue
+               fourthwhere=iangwhere[thirdwhat][l]+thirdwhere
                itorsion=self.calculate_da(firstwhat,secondwhat,thirdwhat,fourthwhat,\
-	       firstwhere,thirdwhere,fourthwhere,directs,lattmat)
-	       if itorsion!=None:
-	         torwhat=[firstwhat,secondwhat,thirdwhat,fourthwhat]
-		 torwhattags=[self.atomictags[firstwhat],self.atomictags[secondwhat],\
-		 self.atomictags[thirdwhat],self.atomictags[fourthwhat]]
-	         torwhere=[firstwhere,secondwhere,thirdwhere,fourthwhere]
+               firstwhere,thirdwhere,fourthwhere,directs,lattmat)
+               if itorsion!=None:
+                 torwhat=[firstwhat,secondwhat,thirdwhat,fourthwhat]
+                 torwhattags=[self.atomictags[firstwhat],self.atomictags[secondwhat],\
+                 self.atomictags[thirdwhat],self.atomictags[fourthwhat]]
+                 torwhere=[firstwhere,secondwhere,thirdwhere,fourthwhere]
                  torsions.append(datastruct.Complextype('simple',[1],tag,torwhat,\
-		 torwhattags,torwhere,itorsion,'free'))
+                 torwhattags,torwhere,itorsion,'free'))
    return torsions
 
  def calculate_da(self,firstwhat,secondwhat,thirdwhat,fourthwhat,\
-	     firstwhere,thirdwhere,fourthwhere,directs,lattmat):
+                  firstwhere,thirdwhere,fourthwhere,directs,lattmat):
    """Calculates dihedral angles.
    """
    if firstwhat==secondwhat or firstwhat==thirdwhat or firstwhat==fourthwhat \
@@ -611,7 +615,7 @@ class Intern:
        fract[index1].append(index2)
      self.topmap[index1].append(index2)
      self.topmap[index2].append(index1)
-   
+
    self.topology_matrix_adds=1*self.topology_matrix
 
    for i in range(numofatoms):
@@ -619,9 +623,9 @@ class Intern:
       dummy=0
       for j in range(numofatoms):
         if self.topology[i,j]==1:
-	  dummy=1
-	  for k in range(numofatoms):
-	    if self.topology[i,k]==1 and self.topology[j,k]==0 :self.topology[j,k]=1
+          dummy=1
+          for k in range(numofatoms):
+            if self.topology[i,k]==1 and self.topology[j,k]==0 :self.topology[j,k]=1
             if self.topology[i,k]==2:self.topology[j,k]=2
       if dummy==1:self.topology[i]=0
 
@@ -694,7 +698,3 @@ class Intern:
        indx=3*i+j
        singles[indx]=cartusa[j]
    return singles
-   
-
-
-
