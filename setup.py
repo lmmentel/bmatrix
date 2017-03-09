17a2717112e08c03a5369a86c1d8c947627cd550
Lukasz Mentel <lmmentel@gmail.com>
2017-03-09 16:48:52 +0100

    added requirements

--- setup.py
+++ setup.py
@@ -1,30 +1,31 @@
 
 ''' writeBmat setup script'''
 
-import sys
-
 from setuptools import setup
+
 
 def readme():
     '''Return the contents of the README.md file.'''
     with open('README.rst') as freadme:
         return freadme.read()
 
+
 setup(
-    author='Tomas Bucko',
-    include_package_data = True,
-    entry_points = {
-        'console_scripts' : [
+    author='Tomas Bucko, Łukasz Mentel',
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
             'writeBmat = writeBmat.writeBmat:main',
         ]
     },
     license='GPLv3',
-    long_description = readme(),
-    name = 'writeBmat',
-    packages = ['writeBmat'],
-    url = 'https://bitbucket.org/lukaszmentel/writebmat/',
-    version = '0.1.0',
-    classifiers = [
+    long_description=readme(),
+    name='writeBmat',
+    packages=['writeBmat'],
+    url='https://bitbucket.org/lukaszmentel/writebmat/',
+    version='0.1.0',
+    install_requires=['numpy', 'scipy'],
+    classifiers=[
         'Environment :: Console',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 2',
