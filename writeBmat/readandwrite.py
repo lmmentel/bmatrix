a465c6c4e22127c054f266512144718429e313fb
Lukasz Mentel <lmmentel@gmail.com>
2018-03-03 15:59:57 +0100

    corrected some synatx errors

--- writeBmat/readandwrite.py
+++ writeBmat/readandwrite.py
@@ -30,52 +30,54 @@ def cart_dir(lvect, carts):
 
 
 def write_whatwhere(coords,fname):
-  wfile=open(fname,'w')
-  for i in range(len(coords)):
-    toadd=coords[i].tag+chr(58)
-    for j in range(len(coords[i].what)):
-      toadd=toadd+chr(32)+str(coords[i].what[j])
-    toadd=toadd+chr(58)
-    for j in range(len(coords[i].where)):
-      for k in range(len(coords[i].where[j])):
-        toadd=toadd+chr(32)+str(coords[i].where[j][k])
-    wfile.write(toadd+'\n')
-  wfile.close()
+    wfile=open(fname,'w')
+    for i in range(len(coords)):
+      toadd=coords[i].tag+chr(58)
+      for j in range(len(coords[i].what)):
+        toadd=toadd+chr(32)+str(coords[i].what[j])
+      toadd=toadd+chr(58)
+      for j in range(len(coords[i].where)):
+        for k in range(len(coords[i].where[j])):
+          toadd=toadd+chr(32)+str(coords[i].where[j][k])
+      wfile.write(toadd+'\n')
+    wfile.close()
+
 
 def read_whatwhere(fname):
-  """Reads definition of primitive
-  coordinate, i.e., the orders
-  of the atoms involved in the coord.
-  """
-  wfile=open(fname,'r')
-  inttags=[]
-  intwhat=[]
-  intwhere=[]
-  for line in wfile.readlines():
-    line=string.split(line,chr(58))
-    tagline=line[0]
-    inttags.append(tagline)
-    whatline=line[1].split()
-    whereline=line[2].split()
-    if whatline[0]=='None':
-      whatline=[None]
-    else:
-      for i in range(len(whatline)):
-        whatline[i]=int(whatline[i])
-    intwhat.append(whatline)
-    if whereline[0]=='None':
-      delement=[[None]]
-    else:
-      for i in range(len(whereline)):
-        whereline[i]=int(whereline[i])
-      k=len(whereline)/3
-      delement=[]
-      for j in range(k):
-        element = np.array([whereline[j*3],whereline[j*3+1],whereline[j*3+2]])
-        delement.append(element)
-    intwhere.append(delement)
-  wfile.close()
-  return inttags,intwhat,intwhere
+    """Reads definition of primitive
+    coordinate, i.e., the orders
+    of the atoms involved in the coord.
+    """
+    wfile=open(fname,'r')
+    inttags=[]
+    intwhat=[]
+    intwhere=[]
+    for line in wfile.readlines():
+      line=string.split(line,chr(58))
+      tagline=line[0]
+      inttags.append(tagline)
+      whatline=line[1].split()
+      whereline=line[2].split()
+      if whatline[0]=='None':
+        whatline=[None]
+      else:
+        for i in range(len(whatline)):
+          whatline[i]=int(whatline[i])
+      intwhat.append(whatline)
+      if whereline[0]=='None':
+        delement=[[None]]
+      else:
+        for i in range(len(whereline)):
+          whereline[i]=int(whereline[i])
+        k=len(whereline)/3
+        delement=[]
+        for j in range(k):
+          element = np.array([whereline[j*3],whereline[j*3+1],whereline[j*3+2]])
+          delement.append(element)
+      intwhere.append(delement)
+    wfile.close()
+    return inttags,intwhat,intwhere
+
 
 def read_cwhatwhere(fname,crt,lattmat):
   """Reads definition of primitive
@@ -177,189 +179,200 @@ def read_cwhatwhere(fname,crt,lattmat):
   wfile.close()
   return inttags,intwhat,intwhere,intcoefs,intstat,complextype
 
-def write_poscar(filename, cartcoords1, lattmat_, inpt):
-  cartcoords3 = np.array(cartcoords1).reshape(len(cartcoords1) / 3, 3)
-  fractcoords3=cart_dir(lattmat_,cartcoords3)
-  #fractcoords3=dot(cartcoords3,inverse(lattmat_))
-  lattmat=lattmat_*pC['AU2A']
-  #cartcoords3=cartcoords3*pC['AU2A']
 
-  kkk=open(filename,'w')
-  kkk.write(inpt.comment+'\n')
-  kkk.write(str(1.000)+'\n')
-  for i in range(len(lattmat)):
-    row='   '+'%.12f'%(round(lattmat[i][0],12))+'   '+'%.12f'%(round(lattmat[i][1],12))+'   '+'%.12f'%(round(lattmat[i][2],12))+'\n'
+def write_poscar(filename, cartcoords1, lattmat_, inpt):
+    cartcoords3 = np.array(cartcoords1).reshape(len(cartcoords1) / 3, 3)
+    fractcoords3=cart_dir(lattmat_,cartcoords3)
+    #fractcoords3=dot(cartcoords3,inverse(lattmat_))
+    lattmat=lattmat_*pC['AU2A']
+    #cartcoords3=cartcoords3*pC['AU2A']
+
+    kkk=open(filename,'w')
+    kkk.write(inpt.comment+'\n')
+    kkk.write(str(1.000)+'\n')
+    for i in range(len(lattmat)):
+      row='   '+'%.12f'%(round(lattmat[i][0],12))+'   '+'%.12f'%(round(lattmat[i][1],12))+'   '+'%.12f'%(round(lattmat[i][2],12))+'\n'
+      kkk.write(row)
+    row=""
+    for i in range(inpt.ntypes):
+      row+=str(inpt.types[i])+" "
+    row+='\n'
     kkk.write(row)
-  row=""
-  for i in range(inpt.ntypes):
-    row+=str(inpt.types[i])+" "
-  row+='\n'
-  kkk.write(row)
-  #kkk.write('cartesian \n')
-  kkk.write('direct \n')
-  for i in range(inpt.numofatoms):
-    row=''
-    for j in range(3):
-      #row=row+'   '+'%.12f'%(round(cartcoords3[i][j],12))
-      row=row+'   '+'%.8f'%(round(fractcoords3[i][j],8))
-    kkk.write(row+'\n')
-  kkk.close()
+    #kkk.write('cartesian \n')
+    kkk.write('direct \n')
+    for i in range(inpt.numofatoms):
+      row=''
+      for j in range(3):
+        #row=row+'   '+'%.12f'%(round(cartcoords3[i][j],12))
+        row=row+'   '+'%.8f'%(round(fractcoords3[i][j],8))
+      kkk.write(row+'\n')
+    kkk.close()
+
 
 def read_store(what,NFREE):
-  name='store.'+what
-  store=open(name,'r')
-  field=[]
-  for line in store.readlines():
-    prefield=line.split()
-    gi=[]
-    for i in range(len(prefield)):
-      gi.append(float(prefield[i]))
-    field.append(gi)
-  store.close()
-  if len(field)>NFREE:
-    field=field[len(field)-NFREE:len(field)]
-  return field
+    name='store.'+what
+    store=open(name,'r')
+    field=[]
+    for line in store.readlines():
+      prefield=line.split()
+      gi=[]
+      for i in range(len(prefield)):
+        gi.append(float(prefield[i]))
+      field.append(gi)
+    store.close()
+    if len(field)>NFREE:
+      field=field[len(field)-NFREE:len(field)]
+    return field
+
 
 def write_store(internalvalues,name):
-  """Writes  to the end of
-  store.x in which all optim. history
-  is stored.
-  """
-  store=open(name,'a')
-  toadd=''
-  for i in range(len(internalvalues)):
-    toadd=toadd+chr(32)+str(internalvalues[i])
-  store.write(toadd+'\n')
-  store.close()
+    """Writes  to the end of
+    store.x in which all optim. history
+    is stored.
+    """
+    store=open(name,'a')
+    toadd=''
+    for i in range(len(internalvalues)):
+        toadd=toadd+chr(32)+str(internalvalues[i])
+    store.write(toadd+'\n')
+    store.close()
 
 
-def write_pstore(coords,name):
-  """Writes prim. internals to the end of
-  store.xi in which all optim. history
-  is stored.
-  """
-  store=open(name,'a')
-  toadd=''
-  for i in range(len(coords)):
-    toadd=toadd+chr(32)+str(coords[i].value)
-  store.write(toadd+'\n')
-  store.close()
+def write_pstore(coords, name):
+    """Writes prim. internals to the end of
+    store.xi in which all optim. history
+    is stored.
+    """
+    store=open(name,'a')
+    toadd=''
+    for i in range(len(coords)):
+        toadd=toadd+chr(32)+str(coords[i].value)
+    store.write(toadd+'\n')
+    store.close()
 
 
-def write_constrained(fname,xconstrained,numofatoms):
-  cfile=open(fname,'w')
-  for i in xconstrained:
-    row = np.zeros(3*numofatoms, dtype=float)
-    row[i]=1
-    wrow=''
-    for j in range(len(row)):
-      wrow=wrow+`row[j]`+chr(32)
-    cfile.write(wrow+'\n')
-  cfile.close()
+def write_constrained(fname, xconstrained, numofatoms):
+    cfile = open(fname, 'w')
+    for i in xconstrained:
+        row = np.zeros(3 * numofatoms, dtype=float)
+        row[i] = 1
+        wrow = ''
+        for j in range(len(row)):
+            wrow = wrow + 'row[j]' + chr(32)
+        cfile.write(wrow + '\n')
+    cfile.close()
+
 
 def read_constrained(cfile=''):
-  cmodes=[]
-  if cfile!='':
-    try:
-      cfile=open(cfile,'r')
-      for line in cfile.readlines():
-        xxx=[]
-        prefield=line.split()
-        for i in range(len(prefield)):
-          xxx.append(float(prefield[i]))
-        cmodes.append(xxx)
-      cfile.close()
-    except IOError:
-      return cmodes
-    else:
-      print('FILE WITH CONSTRAINED fractional coordinates read in')
-  return cmodes
+    cmodes=[]
+    if cfile!='':
+      try:
+        cfile=open(cfile,'r')
+        for line in cfile.readlines():
+          xxx=[]
+          prefield=line.split()
+          for i in range(len(prefield)):
+            xxx.append(float(prefield[i]))
+          cmodes.append(xxx)
+        cfile.close()
+      except IOError:
+        return cmodes
+      else:
+        print('FILE WITH CONSTRAINED fractional coordinates read in')
+    return cmodes
+
 
 def read_atoms(cfile='ATOMS'):
-  atoms=open(cfile,'r')
-  line=atoms.readline()
-  prefield=line.split()
-  return prefield
+    atoms=open(cfile,'r')
+    line=atoms.readline()
+    prefield=line.split()
+    return prefield
+
 
 def read_matrix(cfile):
-  ufile=open(cfile,'r')
-  matrix=pickle.load(ufile)
-  ufile.close()
-  return matrix
+    ufile=open(cfile,'r')
+    matrix=pickle.load(ufile)
+    ufile.close()
+    return matrix
+
 
 def write_matrix(matrix,cfile):
-  ufile=open(cfile,'w')
-  pickle.dump(matrix,ufile)
-  ufile.close()
+    ufile=open(cfile,'w')
+    pickle.dump(matrix,ufile)
+    ufile.close()
+
 
 def write_maxstep(MAXSTEP,cfile):
-  tfile=open(cfile,'w')
-  tfile.write('{0:15.10f}'.format(MAXSTEP))
-  tfile.close()
+    tfile=open(cfile,'w')
+    tfile.write('{0:15.10f}'.format(MAXSTEP))
+    tfile.close()
+
 
 def read_maxstep(cfile):
-  tfile=open(cfile,'r')
-  MAXSTEP=tfile.readline()
-  tfile.close()
-  #MAXSTEP=MAXSTEP[:len(MAXSTEP)-1]
-  MAXSTEP=string.strip(MAXSTEP)
-  MAXSTEP=float(MAXSTEP)
-  return MAXSTEP
+    tfile=open(cfile,'r')
+    MAXSTEP=tfile.readline()
+    tfile.close()
+    #MAXSTEP=MAXSTEP[:len(MAXSTEP)-1]
+    MAXSTEP=string.strip(MAXSTEP)
+    MAXSTEP=float(MAXSTEP)
+    return MAXSTEP
 
 
 def read_hmat():
-  hfile=open('hmat','r')
-  prefield=hfile.readline()
-  prefield=prefield.split()
-  Hdim=int((len(prefield))**0.5)
-  Hmat = np.zeros((Hdim,Hdim), dtype=float)
-  for i in range(Hdim):
-    for j in range(Hdim):
-      num=i*Hdim+j
-      Hmat[i][j]=float(prefield[num])
-  hfile.close()
-  return np.array(Hmat)
+    hfile=open('hmat','r')
+    prefield=hfile.readline()
+    prefield=prefield.split()
+    Hdim=int((len(prefield))**0.5)
+    Hmat = np.zeros((Hdim,Hdim), dtype=float)
+    for i in range(Hdim):
+      for j in range(Hdim):
+        num=i*Hdim+j
+        Hmat[i][j]=float(prefield[num])
+    hfile.close()
+    return np.array(Hmat)
+
 
 def write_hmat(hnext):
-  hfile=open('hmat','w')
-  for i in range(len(hnext)):
-    for j in range(len(hnext)):
-      hfile.write(`hnext[i][j]`+chr(32))
-  hfile.close()
+    hfile=open('hmat','w')
+    for i in range(len(hnext)):
+      for j in range(len(hnext)):
+        hfile.write('hnext[i][j]'+chr(32))
+    hfile.close()
 
 
 def read_umat(cfile='UMAT'):
-  utrans=[]
-  ufile=open(cfile,'r')
-  for line in ufile.readlines():
-    line=line.split()
-    for i in range(len(line)):
-      line[i]=float(line[i])
-    utrans.append(line)
-  ufile.close()
-  utrans = np.array(utrans)
-  return utrans
+    utrans=[]
+    ufile=open(cfile,'r')
+    for line in ufile.readlines():
+      line=line.split()
+      for i in range(len(line)):
+        line[i]=float(line[i])
+      utrans.append(line)
+    ufile.close()
+    utrans = np.array(utrans)
+    return utrans
 
 
 def write_umat(utrans,cfile='UMAT'):
-  ufile=open(cfile,'w')
-  for i in range(len(utrans)):
-    row=''
-    for j in range(len(utrans[0])):
-      row=row+chr(32)+`utrans[i][j]`
-    if i==len(utrans)-1:
-      ufile.write(row)
-    else:
-      ufile.write(row+'\n')
-  ufile.close()
+    ufile=open(cfile,'w')
+    for i in range(len(utrans)):
+      row=''
+      for j in range(len(utrans[0])):
+        row=row+chr(32)+'utrans[i][j]'
+      if i==len(utrans)-1:
+        ufile.write(row)
+      else:
+        ufile.write(row+'\n')
+    ufile.close()
+
 
 def read_translations(cfile):
-  ufile=open(cfile,'r')
-  translations=[]
-  for line in ufile.readlines():
-    line=line.split()
-    for i in range(len(line)):
-      line[i]=int(line[i])
-    translations.append(line)
-  ufile.close()
-  return translations
+    ufile=open(cfile,'r')
+    translations=[]
+    for line in ufile.readlines():
+      line=line.split()
+      for i in range(len(line)):
+        line[i]=int(line[i])
+      translations.append(line)
+    ufile.close()
+    return translations
