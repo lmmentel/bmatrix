61be85b9531af581696321e4299c2d5b21991ed3
Lukasz Mentel <lmmentel@gmail.com>
2021-08-14 16:29:36 +0200

    updated dependencies

--- setup.py
+++ setup.py
@@ -1,37 +1,36 @@
-
-''' writeBmat setup script'''
+""" bmatrix setup script"""
 
 from setuptools import setup
 
 
 def readme():
-    '''Return the contents of the README.md file.'''
-    with open('README.rst') as freadme:
+    """Return the contents of the README.md file."""
+    with open("README.rst") as freadme:
         return freadme.read()
 
 
 setup(
-    author='Tomas Bucko, Łukasz Mentel',
+    author="Tomas Bucko, Łukasz Mentel",
     include_package_data=True,
     entry_points={
-        'console_scripts': [
-            'writeBmat = writeBmat.writeBmat:main',
+        "console_scripts": [
+            "bmatrix = bmatrix.bmatrix:main",
         ]
     },
-    license='GPLv3',
+    license="GPLv3",
     long_description=readme(),
-    name='writeBmat',
-    packages=['writeBmat'],
-    url='https://bitbucket.org/lukaszmentel/writebmat/',
-    version='1.0.1',
-    install_requires=['numpy', 'scipy'],
+    name="bmatrix",
+    packages=["bmatrix"],
+    url="https://github.com/lmmentel/bmatrix",
+    version="1.0.1",
+    install_requires=["numpy", "scipy"],
     classifiers=[
-        'Environment :: Console',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Scientific/Engineering :: Chemistry',
-        'Topic :: Scientific/Engineering :: Physics',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        "Environment :: Console",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Scientific/Engineering :: Physics",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
 )
